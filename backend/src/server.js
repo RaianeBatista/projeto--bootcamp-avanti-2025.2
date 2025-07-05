817014b77218f3cf9864083f886931eb31421216
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-07-05 15:32:32 -0300

    refactor: - Cria sistema de rotas modularizado em src/routers/index.js

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import express, { request, response } from "express";
+import { router } from "./routers/index.js";
+
+const app = express();
+app.use(express.json());
+app.use(router)
+
+
+
+app.listen(8080, () => {
+    console.log("Running on port 8080");
+});
